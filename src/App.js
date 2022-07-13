79058402dd068a60815054cb3452f4af8684a7b3
James Brining <james.brining@streets-heaver.com>
2022-07-13 15:10:19 +0100

    Added react router and new routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {"-->"}
+            <li>
+              <Link to="/sample">Another page</Link>
+            </li>
+            {"---->"}
+            <li>
+              <Link to="/sample/nested">Nested page</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/sample" component={AnotherPage} />
+          <Route path="/sample/nested" component={NestedPage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
+const Home = (props) => <div>Home</div>;
+const AnotherPage = (props) => <div>Another page</div>;
+const NestedPage = (props) => <div>Nested page</div>;
 export default App;
